fdb805a2bba4b26f1b73a15337d0daa9707154a2
dmitry <piervov00@gmail.com>
2022-12-12 09:33:52 +0300

    image load

--- app/src/main/java/com/example/testappsubtotal/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/testappsubtotal/ui/list/ListViewModel.kt
@@ -1,17 +1,13 @@
 package com.example.testappsubtotal.ui.list
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.example.testappsubtotal.data.repository.BooksRepositoryImpl
 import com.example.testappsubtotal.model.Books
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import retrofit2.http.Query
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,10 +17,6 @@ class ListViewModel @Inject constructor(private val repository: BooksRepositoryI
     val bookList: LiveData<Books> get() = _booksList
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
-
-    init {
-        Log.d("develop", "init")
-    }
 
     fun getBooksList(query: String) {
         _isLoading.value = true
